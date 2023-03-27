bf9ab2e4be12077414fa0f105eb44315e79e01bd
Pawel C <c.pawel.10@gmail.com>
2023-03-27 15:41:09 -0700

    Fixed leak of pointer; Filled out rest of functions;Test on windows

--- AssignCh10-11/TestScores/TestScoresMain.cpp
+++ AssignCh10-11/TestScores/TestScoresMain.cpp
@@ -10,7 +10,7 @@ void sortAscending(float *list,int size);
 void calculateAverage(float* list, int size, float* average);
 void print(float* list, int size, float* average);
 int main() {
-	float* tListPtr = NULL;
+	float* tListPtr = nullptr;
 	float* averagePtr = new float;
 	int studentCount;
 	do {
@@ -21,8 +21,8 @@ int main() {
 	tListPtr = new float[studentCount];
 	fillArray(tListPtr, studentCount);
 	sortAscending(tListPtr, studentCount);
-	//calculateAverage(tListPtr, studentCount, averagePtr);
-	//print(tListPtr, studentCount, averagePtr);
+	calculateAverage(tListPtr, studentCount, averagePtr);
+	print(tListPtr, studentCount, averagePtr);
 	
 
 
@@ -38,21 +38,40 @@ void fillArray(float * list, int size) {
 			cout << "\nPlease enter a valid grade from 0 to 100: ";
 			cin >> grades;
 		} while (grades<0 || grades>100);
+	
 		*(list+i) = grades;
 		cout << "Entered";
 	}
 }
+
 void sortAscending(float* list, int size) {
 	//create a variable to help swap numbers
 	float temp;
 	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - i; j++) {//the nested loop always shrinks as i increases since the last element after each run will be in place
+		for (int j = 0; j < size-i-1; j++) {//the nested loop always shrinks as i increases since the last element after each run will be in place
 			if (list[j] > list[j + 1]) {//when an element is bigger then the element after it
 				temp = list[j];
 				list[j] = list[j + 1];
 				list[j + 1] = temp;
-			}//the two are swapped and the element swapped goes on to be tested with the next, therefore guranteeing that the gretestelement is at the end
-		}
+				cout<<endl<<list[j];
+				cout<<endl<<list[j+1];
 
+			}//the two are swapped and the element swapped goes on to be tested with the next, therefore guranteeing that the gretestelement is at the end
+			}
+
+		}
 	}
+
+void calculateAverage(float* list, int size, float* average){
+	*(average) = 0;
+	for(int i=0;i<size;i++){
+		*(average)+=*(list+i);
+	}
+	*(average)/=size;
+}
+void print(float* list, int size, float* average){
+	for(int i=0;i<size;i++){
+		cout<<"\nGrade " << i+1 << ": " << *(list+i);
+	}
+	cout<<"\nClass average: " << *(average);
 }
